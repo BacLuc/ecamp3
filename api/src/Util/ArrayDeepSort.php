00b6a73711b10aed0337db54d7412f44c2cd66f2
BacLuc <lucius.bachmann@gmx.ch>
2023-08-29 11:49:58 +0200

    api: add snapshot tests for the api responses
    - test the root endpoint without escaping the values
    - test GetCollection endpoints with escaping the values
    - test GetItem endpoints with escaping the values
    Here using a map to use a fixture per collection endpoint was necessary,
    because taking the first item of the collection endpoint would result in a different
    item used which may have another structure. (e.g. more children or more related entities)
    - test that Patch and GetItem result in the same response
    This was not possible for the /profile endpoint because there some properties are present in
    _links, in PATCH, but not in _embedded, but they still lead to a compatible response.

---
+++ api/src/Util/ArrayDeepSort.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Util;
+
+class ArrayDeepSort {
+    public static function sort(mixed $input): mixed {
+        if (!is_array($input)) {
+            return $input;
+        }
+        $sorted_children = array_map([ArrayDeepSort::class, 'sort'], $input);
+
+        if (array_is_list($sorted_children)) {
+            uasort($sorted_children, function ($a, $b) {
+                $valueStringA = json_encode($a);
+                $valueStringB = json_encode($b);
+
+                return strcmp($valueStringA, $valueStringB);
+            });
+
+            return array_values($sorted_children);
+        }
+
+        ksort($sorted_children);
+
+        return $sorted_children;
+    }
+}
