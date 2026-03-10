95e6155acdf5cc65d637d295874ec0b8a6b17ea6
BacLuc <lucius.bachmann@gmx.ch>
2026-03-10 21:58:25 +0100

    api: switch to correct base class for the RefreshToken Entity
    Since gesdinet/jwt-refresh-token-bundle 1.0: The "Gesdinet\JWTRefreshTokenBundle\Entity\AbstractRefreshToken" class is deprecated, use "Gesdinet\JWTRefreshTokenBundle\Model\AbstractRefreshToken" instead.

--- api/src/Entity/RefreshToken.php
+++ api/src/Entity/RefreshToken.php
@@ -3,8 +3,8 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
+use Gesdinet\JWTRefreshTokenBundle\Model\AbstractRefreshToken;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'refresh_tokens')]
-class RefreshToken extends BaseRefreshToken {}
+class RefreshToken extends AbstractRefreshToken {}
