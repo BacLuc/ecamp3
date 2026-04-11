92638ca88e9d4ef35a656307efdcf59194b3a98e
BacLuc <lucius.bachmann@clubpage.ch>
2026-04-12 01:00:32 +0200

    e2e: only run zz-createCamps.cy.js

--- e2e/specs/zz-createCamp.cy.js
+++ e2e/specs/zz-createCamp.cy.js
@@ -9,10 +9,6 @@ in2Days.setDate(in2Days.getDate() + 2)
 const campTitle = 'title'
 describe('create new camp', () => {
   it('without prototype', function () {
-    if (Cypress.browser.name === 'edge') {
-      console.log('edge has problems since the update to vue3.')
-      this.skip()
-    }
 
     cy.login(bipiUser)
 
